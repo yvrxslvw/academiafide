6c1b79224857da019a6e3f73cd5a1db878346c7a
yvrxslvw <yvrxslvv@gmail.com>
2023-11-18 12:58:23 +0100

    Create login Form entity

--- packages/client/src/widgets/login/UI/Window/index.tsx
+++ packages/client/src/widgets/login/UI/Window/index.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 import { Title } from 'shared';
 import cl from './style.module.scss';
+import { LoginEntities } from 'entities';
 
 export const Window: FC = () => {
+	const { Form } = LoginEntities;
+
 	return (
 		<div className={cl.Container}>
 			<div className={cl.Window}>
 				<Title className={cl.Title}>Autorización</Title>
+				<Form />
 			</div>
 		</div>
 	);
