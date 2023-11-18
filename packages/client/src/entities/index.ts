a966e68ec120ccfa2a5907b07c237b597c386e17
yvrxslvw <yvrxslvv@gmail.com>
2023-11-18 14:52:13 +0100

    Create logup entity module

--- packages/client/src/entities/index.ts
+++ packages/client/src/entities/index.ts
@@ -1,3 +1,4 @@
 export * from './layout';
 export * from './main';
 export * from './login';
+export * from './logup';
--- packages/client/src/widgets/logup/UI/Window/index.tsx
+++ packages/client/src/widgets/logup/UI/Window/index.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
+import { Title } from 'shared';
+import cl from './style.module.scss';
 
 export const Window: FC = () => {
 	return (
-		<>
-			
-		</>
+		<div className={cl.Container}>
+			<div className={cl.Window}>
+				<Title className={cl.Title}>Registro</Title>
+			</div>
+		</div>
 	);
 };
