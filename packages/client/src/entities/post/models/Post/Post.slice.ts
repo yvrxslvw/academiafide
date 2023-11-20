9a9140889bb7dbcd2de1e06628de139d601e9022
yvrxslvw <yvrxslvv@gmail.com>
2023-11-20 11:27:30 +0100

    Temporarily add news widget logic

--- packages/client/src/entities/post/models/Post/Post.slice.ts
+++ packages/client/src/entities/post/models/Post/Post.slice.ts
@@ -7,7 +7,24 @@ export interface PostState {
 }
 
 const initialState: PostState = {
-	entries: {},
+	entries: {
+		1: {
+			id: 1,
+			title: 'I AM TITLE',
+			content: 'I AM CONTENT',
+		},
+		2: {
+			id: 2,
+			title: 'Lorem ipsum',
+			content:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit.\nNesciunt laboriosam voluptatem harum quibusdam repellendus non vero.\nDignissimos iste consequatur aspernatur sapiente ut qui similique, quibusdam est, nihil aut obcaecati delectus.\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laboriosam voluptatem harum quibusdam repellendus non vero. Dignissimos iste consequatur aspernatur sapiente ut qui similique, quibusdam est, nihil aut obcaecati delectus.',
+		},
+		3: {
+			id: 3,
+			title: 'Links',
+			content: 'https://github.com/yvrxslvw\n[Text link|https://github.com/yvrxslvw]',
+		},
+	},
 };
 
 const PostSlice = createSlice({
--- packages/client/src/widgets/news/UI/NewsList/index.tsx
+++ packages/client/src/widgets/news/UI/NewsList/index.tsx
@@ -1,34 +1,19 @@
 import { FC } from 'react';
-import { Title } from 'shared';
+import { Title, modelEntries, useAppSelector } from 'shared';
 import { PostEntities } from 'entities';
 import { formatContent } from '../../utils';
 import cl from './style.module.scss';
 
-const mock = [
-	{
-		title: 'I AM TITLE!!!',
-		content:
-			'I AM CONTENT\nLorem ipsum dolor sit amet consectetur adipisicing elit.\nVero consequatur sunt quas, laudantium impedit sed incidunt ducimus nesciunt illo!\nIn quasi voluptates quas labore ducimus nihil consequuntur architecto, eius odit.\n[Link|https://t.me/yvrxslw]\nhttps://t.me/yvrxslw\n',
-	},
-	{
-		title: 'Lorem',
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur repellat incidunt nobis eveniet est iste iusto distinctio saepe minima, ducimus nam quos dignissimos, in eum cum obcaecati nostrum accusamus eius?',
-	},
-	{
-		title: 'Hello',
-		content: 'Some content over here',
-	},
-];
-
+// ! To review
 export const NewsList: FC = () => {
+	const { entries } = useAppSelector(state => state.post);
 	const { Post } = PostEntities;
 
 	return (
 		<div className={cl.Container}>
 			<Title className={cl.Title}>Últimas noticias</Title>
 			<section className={cl.PostBody}>
-				{mock.map(({ title, content }, index) => (
+				{modelEntries(entries).map(({ title, content }, index) => (
 					<Post className={cl.Post} title={title} content={formatContent(content)} key={index} />
 				))}
 			</section>
