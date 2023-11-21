638d2109c62ec640ffe5c2720978692f32ab7731
yvrxslvw <yvrxslvv@gmail.com>
2023-11-22 00:27:13 +0100

    Remake post stack tracing system

--- packages/server/src/post/post.controller.ts
+++ packages/server/src/post/post.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Res, Body, UploadedFile, UseInterceptors, Put, Delete, Param, Patch, Get } from '@nestjs/common';
+import { Controller, Body, UploadedFile, UseInterceptors, Put, Delete, Param, Patch, Get } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Response } from 'express';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
@@ -16,18 +15,16 @@ export class PostController {
 	@ApiResponse({ status: 200, description: 'Successfully creating post', type: Post })
 	@Put()
 	@UseInterceptors(FileInterceptor('image'))
-	async create(@Res() res: Response, @Body() postDto: CreatePostDto, @UploadedFile() image?: any) {
-		const data = await this.service.create(postDto, image);
-		res.status(data.getStatus()).send(data.getResponse());
+	create(@Body() postDto: CreatePostDto, @UploadedFile() image?: any) {
+		return this.service.create(postDto, image);
 	}
 
 	@ApiOperation({ summary: 'Post deleting' })
 	@ApiResponse({ status: 200, description: 'Successfully deleting post' })
 	@ApiResponse({ status: 404, description: "If post doesn't exists" })
 	@Delete('/:id')
-	async delete(@Res() res: Response, @Param('id') id: number) {
-		const data = await this.service.delete(id);
-		res.status(data.getStatus()).send(data.getResponse());
+	delete(@Param('id') id: number) {
+		return this.service.delete(id);
 	}
 
 	@ApiOperation({ summary: 'Post updating' })
@@ -35,30 +32,22 @@ export class PostController {
 	@ApiResponse({ status: 404, description: "If post doesn't exists" })
 	@Patch('/:id')
 	@UseInterceptors(FileInterceptor('image'))
-	async update(
-		@Res() res: Response,
-		@Param('id') id: number,
-		@Body() postDto: UpdatePostDto,
-		@UploadedFile() image?: any,
-	) {
-		const data = await this.service.update(id, postDto, image);
-		res.status(data.getStatus()).send(data.getResponse());
+	update(@Param('id') id: number, @Body() postDto: UpdatePostDto, @UploadedFile() image?: any) {
+		return this.service.update(id, postDto, image);
 	}
 
 	@ApiOperation({ summary: 'Getting all posts' })
 	@ApiResponse({ status: 200, description: 'Successfully getting all post', type: [Post] })
 	@Get()
-	async getAll(@Res() res: Response) {
-		const data = await this.service.getAll();
-		res.status(data.getStatus()).send(data.getResponse());
+	getAll() {
+		return this.service.getAll();
 	}
 
 	@ApiOperation({ summary: 'Getting one post by ID' })
 	@ApiResponse({ status: 200, description: 'Successfully getting one post by ID', type: Post })
 	@ApiResponse({ status: 404, description: "If post doesn't exists" })
 	@Get('/:id')
-	async getOneById(@Res() res: Response, @Param('id') id: number) {
-		const data = await this.service.getOneById(id);
-		res.status(data.getStatus()).send(data.getResponse());
+	getOneById(@Param('id') id: number) {
+		return this.service.getOneById(id);
 	}
 }
--- packages/server/src/post/post.service.ts
+++ packages/server/src/post/post.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common/exceptions';
 import { InjectModel } from '@nestjs/sequelize';
 import { Post } from './post.model';
 import { FilesService } from 'src/files/files.service';
@@ -10,65 +11,40 @@ export class PostService {
 	constructor(@InjectModel(Post) private readonly repo: typeof Post, private readonly filesService: FilesService) {}
 
 	async create(postDto: CreatePostDto, image?: any) {
-		try {
-			const fileName = image ? await this.filesService.createFile(image) : null;
-			const post = await this.repo.create({ ...postDto, image: fileName });
-			return new HttpException(post, HttpStatus.OK);
-		} catch (error) {
-			console.error(error);
-			return new HttpException('An unexpected error occurred...', HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		const fileName = image ? await this.filesService.createFile(image) : null;
+		const post = await this.repo.create({ ...postDto, image: fileName });
+		return post;
 	}
 
 	async delete(id: number) {
-		try {
-			const post = await this.repo.findByPk(id);
-			if (!post) return new HttpException('Post not found.', HttpStatus.NOT_FOUND);
-			const fileName = post.image;
-			await post.destroy();
-			if (fileName) await this.filesService.deleteFile(fileName);
-			return new HttpException('Deleted.', HttpStatus.OK);
-		} catch (error) {
-			console.error(error);
-			return new HttpException('An unexpected error occurred...', HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		const post = await this.repo.findByPk(id);
+		if (!post) throw new NotFoundException('Post not found.');
+		const fileName = post.image;
+		await post.destroy();
+		if (fileName) await this.filesService.deleteFile(fileName);
+		return 'Deleted.';
 	}
 
 	async update(id: number, postDto: UpdatePostDto, image?: any) {
-		try {
-			const post = await this.repo.findByPk(id);
-			if (!post) return new HttpException('Post not found.', HttpStatus.NOT_FOUND);
-			if (image) {
-				const fileName = await this.filesService.createFile(image);
-				if (post.image) await this.filesService.deleteFile(post.image);
-				await post.update({ image: fileName });
-			}
-			await post.update({ ...postDto });
-			return new HttpException(post, HttpStatus.OK);
-		} catch (error) {
-			console.error(error);
-			return new HttpException('An unexpected error occurred...', HttpStatus.INTERNAL_SERVER_ERROR);
+		const post = await this.repo.findByPk(id);
+		if (!post) throw new NotFoundException('Post not found.');
+		if (image) {
+			const fileName = await this.filesService.createFile(image);
+			if (post.image) await this.filesService.deleteFile(post.image);
+			await post.update({ image: fileName });
 		}
+		await post.update({ ...postDto });
+		return post;
 	}
 
 	async getAll() {
-		try {
-			const posts = await this.repo.findAll({ include: { all: true, nested: true } });
-			return new HttpException(posts, HttpStatus.OK);
-		} catch (error) {
-			console.error(error);
-			return new HttpException('An unexpected error occurred...', HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		const posts = await this.repo.findAll({ include: { all: true, nested: true } });
+		return posts;
 	}
 
 	async getOneById(id: number) {
-		try {
-			const post = await this.repo.findByPk(id, { include: { all: true, nested: true } });
-			if (!post) return new HttpException('Post not found.', HttpStatus.NOT_FOUND);
-			return new HttpException(post, HttpStatus.OK);
-		} catch (error) {
-			console.error(error);
-			return new HttpException('An unexpected error occurred...', HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		const post = await this.repo.findByPk(id, { include: { all: true, nested: true } });
+		if (!post) throw new NotFoundException('Post not found.');
+		return post;
 	}
 }
