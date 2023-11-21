77ff77e726167e39768d025d2931e9a27f6de33d
yvrxslvw <yvrxslvv@gmail.com>
2023-11-21 16:18:14 +0100

    Create database connection

--- packages/server/src/App.module.ts
+++ packages/server/src/App.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { HelloChessModule } from './hello-chess/hello-chess.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
-const envFilePath = process.env.APP_MODE === 'development' ? '.env.development' : '.env';
+const isDev = process.env.APP_MODE === 'development';
+const envFilePath = isDev ? '.env.development' : '.env';
 
 @Module({
-	imports: [HelloChessModule, ConfigModule.forRoot({ envFilePath })],
+	imports: [
+		ConfigModule.forRoot({ envFilePath }),
+		SequelizeModule.forRoot({
+			dialect: 'mysql',
+			host: process.env.MYSQL_HOST,
+			port: 3306,
+			username: process.env.MYSQL_USERNAME,
+			password: process.env.MYSQL_PASSWORD,
+			database: process.env.MYSQL_DATABASE,
+			timezone: process.env.MYSQL_TIMEZONE,
+			logging: isDev ? sql => console.log(sql) : false,
+			models: [],
+			autoLoadModels: true,
+		}),
+	],
 })
 export class AppModule {}
--- packages/server/src/hello-chess/hello-chess.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HelloChessController } from './hello-chess.controller';
-import { HelloChessService } from './hello-chess.service';
-
-@Module({
-  controllers: [HelloChessController],
-  providers: [HelloChessService]
-})
-export class HelloChessModule {}
