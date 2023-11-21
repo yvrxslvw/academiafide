954db92e5a172d4973249e121c146b06f338214b
yvrxslvw <yvrxslvv@gmail.com>
2023-11-21 15:56:35 +0100

    Create building system

--- packages/server/src/App.module.ts
+++ packages/server/src/App.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HelloChessModule } from './hello-chess/hello-chess.module';
 
+const envFilePath = process.env.APP_MODE === 'development' ? '.env.development' : '.env';
+
 @Module({
-	imports: [HelloChessModule, ConfigModule.forRoot({ envFilePath: `.env.${process.env.APP_MODE}` })],
+	imports: [HelloChessModule, ConfigModule.forRoot({ envFilePath })],
 })
 export class AppModule {}
---
+++ packages/server/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const jetpack = require('fs-jetpack');
+
+module.exports = () => {
+	const buildOutput = path.resolve(__dirname, 'dist');
+	const envFile = path.resolve(__dirname, '.env.production');
+	const pkgFile = path.resolve(__dirname, 'package.json');
+	const pkg = jetpack.read(pkgFile, 'json');
+
+	jetpack.copy(envFile, buildOutput + '/.env');
+	jetpack.write(buildOutput + '/package.json', {
+		name: pkg.name,
+		version: pkg.version,
+		description: pkg.description,
+		author: pkg.author,
+		private: pkg.private,
+		license: pkg.license,
+		scripts: {
+			start: 'node main.js',
+		},
+		dependencies: pkg.dependencies,
+	});
+
+	const config = {
+		mode: 'production',
+		output: {
+			path: buildOutput,
+			filename: '[name].js',
+		},
+	};
+
+	return config;
+};
