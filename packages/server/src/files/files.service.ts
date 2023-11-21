56db75bf1e77c09e2a6e3ca526f6690398b8a492
yvrxslvw <yvrxslvv@gmail.com>
2023-11-22 00:32:02 +0100

    Remake files stack tracing system

--- packages/server/src/files/files.service.ts
+++ packages/server/src/files/files.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as uuid from 'uuid';
 import * as path from 'path';
 import * as fs from 'fs';
@@ -6,26 +6,16 @@ import * as fs from 'fs';
 @Injectable()
 export class FilesService {
 	async createFile(file: any) {
-		try {
-			const fileName = uuid.v4() + '.jpg';
-			const filePath = path.resolve(__dirname, 'static');
-			if (!fs.existsSync(filePath)) fs.mkdirSync(filePath, { recursive: true });
-			fs.writeFileSync(path.join(filePath, fileName), file.buffer);
-			return fileName;
-		} catch (error) {
-			console.error(error);
-			throw new HttpException('An unexpected error occurred...', HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		const fileName = uuid.v4() + '.jpg';
+		const filePath = path.resolve(__dirname, 'static');
+		if (!fs.existsSync(filePath)) fs.mkdirSync(filePath, { recursive: true });
+		fs.writeFileSync(path.join(filePath, fileName), file.buffer);
+		return fileName;
 	}
 
 	async deleteFile(fileName: string) {
-		try {
-			const filePath = path.resolve(__dirname, 'static', fileName);
-			if (!filePath) throw new Error("File doesn't exists");
-			fs.rmSync(filePath);
-		} catch (error) {
-			console.error(error);
-			throw new HttpException('An unexpected error occurred...', HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		const filePath = path.resolve(__dirname, 'static', fileName);
+		if (!filePath) throw new Error("File doesn't exists");
+		fs.rmSync(filePath);
 	}
 }
