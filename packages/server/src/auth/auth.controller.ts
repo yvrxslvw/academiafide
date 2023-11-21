4589877a34d361d195daf326d362a1d3113d0064
yvrxslvw <yvrxslvv@gmail.com>
2023-11-22 00:22:02 +0100

    Create auth module documentation

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -2,16 +2,24 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Authorization')
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly service: AuthService) {}
 
+	@ApiOperation({ summary: 'Authorize user' })
+	@ApiResponse({ status: 201, description: 'Successful authorization and get the token' })
+	@ApiResponse({ status: 403, description: 'Incorrect login or password' })
 	@Post('/login')
 	login(@Body() loginDto: LoginUserDto) {
 		return this.service.login(loginDto);
 	}
 
+	@ApiOperation({ summary: 'Register user' })
+	@ApiResponse({ status: 201, description: 'Successful registration and get the token' })
+	@ApiResponse({ status: 403, description: 'If login or email already exists' })
 	@Post('/logup')
 	logup(@Body() logupDto: CreateUserDto) {
 		return this.service.logup(logupDto);
