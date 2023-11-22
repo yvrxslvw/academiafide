6575bb64f883ec8daa51ccc48e5bc9afa0091439
yvrxslvw <yvrxslvv@gmail.com>
2023-11-22 18:35:11 +0100

    Create get products interactions

--- packages/server/src/products/products.controller.ts
+++ packages/server/src/products/products.controller.ts
@@ -1,9 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ProductsService } from './products.service';
+import { Product } from './product.model';
 
 @ApiTags('Product interactions')
 @Controller('products')
 export class ProductsController {
 	constructor(private readonly productsService: ProductsService) {}
+
+	@ApiOperation({ summary: 'Getting all products' })
+	@ApiResponse({ status: 200, description: 'Successfully getting all products', type: [Product] })
+	@Get()
+	getAll() {
+		return this.productsService.getAll();
+	}
+
+	@ApiOperation({ summary: 'Getting one product by ID' })
+	@ApiResponse({ status: 200, description: 'Successfully getting one product by ID', type: Product })
+	@ApiResponse({ status: 404, description: "If product doesn't exists" })
+	@Get('/:id')
+	getOneById(@Param('id') id: number) {
+		return this.productsService.getOneById(id);
+	}
 }
--- packages/server/src/products/products.service.ts
+++ packages/server/src/products/products.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Product } from './product.model';
 
 @Injectable()
 export class ProductsService {
 	constructor(@InjectModel(Product) private readonly productRepo: typeof Product) {}
+
+	async getAll() {
+		const products = await this.productRepo.findAll();
+		return products;
+	}
+
+	async getOneById(id: number) {
+		const product = await this.productRepo.findByPk(id);
+		if (!product) throw new NotFoundException("Product doesn't exists.");
+		return product;
+	}
 }
