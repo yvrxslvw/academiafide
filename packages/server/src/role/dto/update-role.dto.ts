19dda2ce56ab92f20e3360ffe09d7686fb95b1a4
yvrxslvw <yvrxslvv@gmail.com>
2023-11-22 16:19:31 +0100

    Add documentation for update role dto

--- packages/server/src/role/dto/update-role.dto.ts
+++ packages/server/src/role/dto/update-role.dto.ts
@@ -1,5 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateRoleDto {
+	@ApiProperty({ example: 'NEW', description: 'New role tag', required: false })
 	declare tag?: string;
 
+	@ApiProperty({ example: 'New role', description: 'New role description', required: false })
 	declare description?: string;
 }
