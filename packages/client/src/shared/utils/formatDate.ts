c022b6c07b0b4ff6031389e2d1c1f50a28c36fc9
yvrxslvw <yvrxslvv@gmail.com>
2023-11-25 22:08:51 +0100

    Fix date minor bug

--- packages/client/src/shared/utils/formatDate.ts
+++ packages/client/src/shared/utils/formatDate.ts
@@ -1,4 +1,15 @@
 export const formatDate = (dateFromBackend: string): string => {
-	const date = new Date(dateFromBackend);
-	return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+	const fullDate = new Date(dateFromBackend);
+	let hours: number | string = fullDate.getHours();
+	let minutes: number | string = fullDate.getMinutes();
+	let date: number | string = fullDate.getDate();
+	let month: number | string = fullDate.getMonth() + 1;
+	const fullYear: number | string = fullDate.getFullYear();
+
+	if (hours < 10) hours = `0${hours}`;
+	if (minutes < 10) minutes = `0${minutes}`;
+	if (date < 10) date = `0${date}`;
+	if (month < 10) month = `0${month}`;
+
+	return `${hours}:${minutes} ${date}.${month}.${fullYear}`;
 };
