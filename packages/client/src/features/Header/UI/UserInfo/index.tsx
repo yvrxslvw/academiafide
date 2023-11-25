409934630da7e7dac024c726561b28a69913c649
yvrxslvw <yvrxslvv@gmail.com>
2023-11-25 17:16:21 +0100

    Add logout feature

--- packages/client/src/features/Header/UI/UserInfo/index.tsx
+++ packages/client/src/features/Header/UI/UserInfo/index.tsx
@@ -1,18 +1,28 @@
 import { FC } from 'react';
-import { Link, PublicRouterPaths, formatImageUrl, useAppSelector } from 'shared';
+import { Link, PublicRouterPaths, formatImageUrl, useAppDispatch, useAppSelector } from 'shared';
 import cl from './style.module.scss';
+import { UserSlice } from 'app/store/models';
 
 export const UserInfo: FC = () => {
 	const { userInfo } = useAppSelector(state => state.user);
+	const dispatch = useAppDispatch();
+	const { logout } = UserSlice.actions;
 
 	const onClickAvatarHandler = () => {
 		// eslint-disable-next-line no-console
 		console.info('will be account actions here');
 	};
 
+	const onClickLogoutHandler = () => {
+		dispatch(logout());
+		window.localStorage.removeItem('accessToken');
+	};
+
 	return (
 		<>
-			<Link to={PublicRouterPaths.MAIN_PAGE}>Cerrar sesión</Link>
+			<Link to={PublicRouterPaths.MAIN_PAGE} onClick={onClickLogoutHandler}>
+				Cerrar sesión
+			</Link>
 			<img
 				src={userInfo.image ? formatImageUrl(userInfo.image) : 'https://via.placeholder.com/600/92c952'}
 				alt='Avatar'
