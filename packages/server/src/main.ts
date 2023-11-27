d6130dc8d85849792093955b5a55644df0d4a26c
yvrxslvw <yvrxslvv@gmail.com>
2023-11-27 02:52:35 +0100

    Add auto-creating basic roles when starting the server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './App.module';
+import { Role } from './roles/role.model';
 
 const bootstrap = async () => {
 	const HOST = process.env.API_HOST ?? 'localhost';
@@ -10,7 +11,7 @@ const bootstrap = async () => {
 	const app = await NestFactory.create(AppModule, { cors: { origin: CLIENT_URL } });
 
 	app.setGlobalPrefix('/api');
-	
+
 	const swaggerConfig = new DocumentBuilder()
 		.setTitle('Academia Fide backend server')
 		.setDescription('This is REST API backend server documentation for Academia Fide.')
@@ -20,8 +21,12 @@ const bootstrap = async () => {
 	SwaggerModule.setup('/api/documentation', app, swaggerDocument);
 
 	app.useGlobalPipes(new ValidationPipe());
-	await app.listen(PORT, HOST, async () =>
-		console.log(`The backend server has been started on ${await app.getUrl()}.`),
-	);
+	await app.listen(PORT, HOST, async () => {
+		console.log(`The backend server has been started on ${await app.getUrl()}.`);
+		const userRole = await Role.findOne({ where: { tag: 'USER' } });
+		const adminRole = await Role.findOne({ where: { tag: 'ADMIN' } });
+		if (!userRole) await Role.create({ tag: 'USER', description: 'User' });
+		if (!adminRole) await Role.create({ tag: 'ADMIN', description: 'Administrator' });
+	});
 };
 bootstrap();
