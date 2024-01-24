f6402f74d6c20b9b0e7520f7df1ac71024a03f73
yvrxslvw <yvrxslvv@gmail.com>
2024-01-24 09:19:16 +0100

    Add image for no image shop item

--- packages/client/src/entities/shop/UI/Item/index.tsx
+++ packages/client/src/entities/shop/UI/Item/index.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import { Paragraph, Title } from 'shared/UI';
+import { Icons } from 'shared/assets';
 import cl from './style.module.scss';
 
 interface ItemProps {
@@ -13,7 +14,13 @@ interface ItemProps {
 export const Item: FC<ItemProps> = ({ imageUrl, title, description, purchaseButton, actionsDropdown }) => {
 	return (
 		<div className={cl.ShopItem}>
-			{imageUrl ? <img src={imageUrl} alt='ShopItem' className={cl.ItemImage} /> : <span />}
+			<section className={cl.ImageSection}>
+				{imageUrl ? (
+					<img src={imageUrl} alt='ShopItem' className={cl.ItemImage} />
+				) : (
+					<img src={Icons.ChessFigure} alt='Shop item' className={cl.NoImage} />
+				)}
+			</section>
 			<section className={cl.Info}>
 				<Title className={cl.Title}>{title}</Title>
 				<Paragraph className={cl.Content}>{description}</Paragraph>
