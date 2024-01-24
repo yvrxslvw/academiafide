bfaff90a73e551ced4624578f73abc2a147b3bcd
yvrxslvw <yvrxslvv@gmail.com>
2024-01-24 09:59:51 +0100

    Complete user list page

--- packages/client/src/entities/user/UI/Row/index.tsx
+++ packages/client/src/entities/user/UI/Row/index.tsx
@@ -1,7 +1,33 @@
 import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { IUser } from 'shared/models';
+import { API_URL, PublicRouterPaths } from 'shared/constants';
+import { Icons } from 'shared/assets';
+import { RoleTag } from 'shared/UI';
+import cl from './style.module.scss';
 
-interface UserRowProps {}
+interface UserRowProps {
+	user: IUser;
+}
 
-export const UserRow: FC<UserRowProps> = () => {
-	return <>meow </>;
+export const UserRow: FC<UserRowProps> = ({ user }) => {
+	const navigate = useNavigate();
+
+	const onClickHandler = () => {
+		navigate(PublicRouterPaths.USERS_PAGE + `/${user.login}`);
+	};
+
+	return (
+		<div className={cl.UserRow} onClick={onClickHandler}>
+			<section className={cl.ImageSection}>
+				<img src={user.image ? API_URL + `/${user.image}` : Icons.ChessFigure} alt='user' />
+			</section>
+			<p>{user.login}</p>
+			<section className={cl.RolesSection}>
+				{user.roles.map(({ id, tag, description }) => (
+					<RoleTag key={id} tag={tag} description={description} />
+				))}
+			</section>
+		</div>
+	);
 };
--- packages/client/src/widgets/users/UI/UsersList/index.tsx
+++ packages/client/src/widgets/users/UI/UsersList/index.tsx
@@ -1,22 +1,24 @@
-import { UserRow } from 'entities/user';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import { UserRow } from 'entities/user';
 import { Loader } from 'shared/UI';
 import { useGetAllUsersQuery } from 'shared/api';
+import cl from './style.module.scss';
 
 export const UsersList: FC = () => {
 	const { data, isLoading, isError } = useGetAllUsersQuery();
 	const { t } = useTranslation();
 
 	return (
-		<>
+		<div className={cl.UsersList}>
+			<h2>{t('Lista de usuarios')}</h2>
 			{isLoading ? (
 				<Loader />
 			) : isError ? (
 				t('Se produjo un error inesperado... Vuelva a intentarlo más tarde.')
 			) : (
-				data && data.map(user => <UserRow key={user.id} />)
+				data && data.map(user => <UserRow key={user.id} user={user} />)
 			)}
-		</>
+		</div>
 	);
 };
