d8bba1743b91ddf47b4655583278bddf36b785a1
yvrxslvw <yvrxslvv@gmail.com>
2024-01-24 07:54:22 +0100

    Remake navigation after click the button on the join card

--- packages/client/src/features/JoinCard/UI/JoinButton/index.tsx
+++ packages/client/src/features/JoinCard/UI/JoinButton/index.tsx
@@ -3,13 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { Button } from 'shared/UI';
 import { PublicRouterPaths } from 'shared/constants';
+import { useAppSelector } from 'shared/hooks';
 
 export const JoinButton: FC = () => {
 	const navigate = useNavigate();
+	const { isLogged, userInfo } = useAppSelector(state => state.user);
 	const { t } = useTranslation();
 
 	const onClickHandler = () => {
-		navigate(PublicRouterPaths.LOGUP_PAGE);
+		navigate(isLogged ? PublicRouterPaths.USERS_PAGE + `/${userInfo.login}` : PublicRouterPaths.LOGUP_PAGE);
 	};
 
 	return <Button onClick={onClickHandler}>{t('Unirse')}</Button>;
